47f9c1c0ca34882c6f453441516a7b0606c8bf77
marcomaiermm <mm.maiermarco@gmail.com>
2023-07-27 18:39:55 +0200

    batman

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,7 +3,19 @@ return {
   "AstroNvim/astrocommunity",
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  
+  -- themes
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  -- motions
+  { import = "astrocommunity.motion.mini-move" },
+  -- editing Support
+  { import = "astrocommunity.editing-support.multicursors-nvim" },
+  { import = "astrocommunity.editing-support.refactoring-nvim" },
+  -- completion
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- git
+  { import = "astrocommunity.git.diffview-nvim" },
+  -- packs
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.python" },
 }
